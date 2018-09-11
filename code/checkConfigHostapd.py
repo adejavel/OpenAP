f728e044d0d12fbdfafa981dbd55869b5c37b5cd
adejavel <adejavel@cisco.com>
2018-09-11 20:26:00 +0200

    changing

--- code/checkConfigHostapd.py
+++ code/checkConfigHostapd.py
@@ -111,7 +111,8 @@ for wifimode in ["b","g"]:
                                 setParameterHostapdConfig("ieee80211n", "1")
                             setParameterHostapdConfig("ht_capab", ht_c)
 
-                            os.system("hostapd -B /etc/hostapd/hostapd_check_conf.conf")
+                            out = os.system("hostapd -B /etc/hostapd/hostapd_check_conf.conf")
+                            logger.info(out)
                             #os.system("hostapd /etc/hostapd/hostapd_check_conf.conf")
                             #output = subprocess.check_output("hostapd /etc/hostapd/hostapd_check_conf.conf", shell=True)
                             #output = subprocess.Popen("/usr/sbin/hostapd /etc/hostapd/hostapd_check_conf.conf")
@@ -188,7 +189,8 @@ for wifimode in ["b","g"]:
                         if wifimode in ["a"]:
                             setParameterHostapdConfig("ieee80211n", "1")
 
-                        os.system("hostapd -B /etc/hostapd/hostapd_check_conf.conf")
+                        out = os.system("hostapd -B /etc/hostapd/hostapd_check_conf.conf")
+                        logger.info(out)
                         #
                         #output = subprocess.check_output("hostapd -B /etc/hostapd/hostapd_check_conf.conf", shell=True)
                         #output = subprocess.Popen("/usr/sbin/hostapd /etc/hostapd/hostapd_check_conf.conf")
