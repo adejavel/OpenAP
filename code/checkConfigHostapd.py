f02226c872687ba38672772fda1adace7824d485
adejavel <adejavel@cisco.com>
2018-09-11 19:50:31 +0200

    changing

--- code/checkConfigHostapd.py
+++ code/checkConfigHostapd.py
@@ -115,7 +115,7 @@ for wifimode in ["b","g","a"]:
                             #output = subprocess.check_output("hostapd /etc/hostapd/hostapd_check_conf.conf", shell=True)
                             #output = subprocess.Popen("hostapd /etc/hostapd/hostapd_check_conf.conf")
                             cmd = ['hostapd', '/etc/hostapd/hostapd_check_conf.conf']
-                            output = subprocess.run("hostapd /etc/hostapd/hostapd_check_conf.conf",timeout=0.2)
+                            output = subprocess.run("hostapd /etc/hostapd/hostapd_check_conf.conf",timeout=0.2,shell=True)
                             #print(output)
                             print("It didn't worked!")
 
@@ -179,7 +179,7 @@ for wifimode in ["b","g","a"]:
                         # output = subprocess.check_output("hostapd /etc/hostapd/hostapd_check_conf.conf", shell=True)
                         # output = subprocess.Popen("hostapd /etc/hostapd/hostapd_check_conf.conf")
                         cmd = ['hostapd', '/etc/hostapd/hostapd_check_conf.conf']
-                        output = subprocess.run("hostapd /etc/hostapd/hostapd_check_conf.conf", timeout=0.2)
+                        output = subprocess.run("hostapd /etc/hostapd/hostapd_check_conf.conf", timeout=0.2,shell=True)
                         # print(output)
                         print("It didn't worked!")
 
