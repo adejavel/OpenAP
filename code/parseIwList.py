e3e527aece9872c6e730f0de01ad028c654a1e3b
adejavel <adejavel@cisco.com>
2018-09-16 19:41:50 +0200

    changing

--- code/parseIwList.py
+++ code/parseIwList.py
@@ -1,4 +1,6 @@
 import subprocess
 output = subprocess.Popen('iw wlan0 info', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for line in output.stdout.readlines():
-    print line
+    if "phy" in line:
+        wlan = line.split()[1]
+print wlan
