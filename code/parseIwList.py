923190f3b7fe271b83abc918d8408c3b585968d4
adejavel <adejavel@cisco.com>
2018-09-16 20:33:32 +0200

    changing

--- code/parseIwList.py
+++ code/parseIwList.py
@@ -16,32 +16,25 @@ for line in output2.stdout.readlines():
     raw= repr(line)
     line2 = line.replace(" ","")
     leading_spaces = len(line2) - len(line2.lstrip())
-    print leading_spaces
-    print raw
     if inFreq:
-        print "IN FREQ"
         if leading_spaces != 3:
             inFreq=False
         else:
             if not "(radar detection)" in raw and not "(disabled)" in raw:
                 fr = (line.split("[")[1]).split("]")[0]
-                print "GOOD STARTTTTT"
                 if inBand1:
                     obj["bgn"].append(int(fr))
                 elif inBand2:
                     obj["a"].append(int(fr))
     if "Band 1" in line:
-        print "BAND 1"
         inBand1=True
         inBand2 = False
         inFreq = False
     elif "Band 2" in line:
-        print "BAND 2"
         inBand1=False
         inBand2 = True
         inFreq = False
     if "Frequencies" in line:
-        print "FREQ"
         inFreq=True
 
 
