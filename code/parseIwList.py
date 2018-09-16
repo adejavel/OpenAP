42416105e0701ad03e57f2464dda99a50fad6325
adejavel <adejavel@cisco.com>
2018-09-16 20:02:24 +0200

    changing

--- code/parseIwList.py
+++ code/parseIwList.py
@@ -5,5 +5,34 @@ for line in output.stdout.readlines():
         wlan = line.split()[1]
 print wlan
 output2 = subprocess.Popen('iw list', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+obj={
+    "2.4":[],
+    "5":[]
+}
+inBand1=False
+inBand2=False
+inFreq=False
 for line in output2.stdout.readlines():
-    print repr(line)
+    raw= repr(line)
+    if "Band 1" in line:
+        inBand1=True
+        inBand2 = False
+        inFreq = False
+    elif "Band 2" in line:
+        inBand1=False
+        inBand2 = True
+        inFreq = False
+    if "Frequencies" in line:
+        inFreq=True
+    if inFreq:
+        if not raw.startswith("\t\t\t"):
+            inFreq=False
+        else:
+            if inBand1:
+                obj["2.4"].append(line)
+            elif inBand2:
+                obj["5"].append(line)
+
+
+
+print obj
