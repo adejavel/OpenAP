e66ed1990fbb1d5709500dd5abc8649bc59959a1
adejavel <adejavel@cisco.com>
2018-09-17 15:00:17 +0200

    changing

--- code/main.py
+++ code/main.py
@@ -323,7 +323,7 @@ def applyConfig():
                         data = json.load(f)
                         logger.info(data)
                     try:
-                        with open('hostapd_available_config.json') as f:
+                        with open('hostapd_available_config.json', encoding='utf-8') as f:
                             channel = getFieldHostapdConfig("channel")
                             data = json.load(f)
                             avai = data["configs"]["a"]["40"][channel]
