eb1c30649e94f312c689c18886ea819399c530d0
adejavel <adejavel@cisco.com>
2018-12-22 12:20:41 +0100

    working on policies

--- autoBoot.py
+++ autoBoot.py
@@ -22,6 +22,7 @@ logger.info("Running autoboot script!")
 
 while True:
     try:
+        subprocess.Popen("python OpenAP/OpenAP/code/applyPolicies", shell=True)
         subprocess.Popen("python OpenAP/OpenAP/code/main.py", shell=True)
         break
     except:
