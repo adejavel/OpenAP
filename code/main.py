8335d3f9d3a674978c98b218b9dd23021c72f163
adejavel <adejavel@cisco.com>
2018-12-25 19:39:27 +0100

    adding v1 usb

--- code/main.py
+++ code/main.py
@@ -48,7 +48,7 @@ def get_config():
 def getStructureUSB():
     try:
         data = os.popen("tree -JafsD /media/pi")
-        jsonData = json.loads(data.read())[0]["contents"]
+        jsonData = json.loads(data.read().encode("utf-8"))[0]["contents"]
         newData=[]
         for elem in jsonData:
             logger.info(elem)
