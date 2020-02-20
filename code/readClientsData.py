82c53a60f11733be83d36ecaf2d3efc2d7bcf8c5
Aymeric de Javel <adejavel@MacBook-Pro-de-Aymeric.local>
2020-02-20 11:13:38 +0100

    adding broadcast ping for client IP recovery

--- code/readClientsData.py
+++ code/readClientsData.py
@@ -49,10 +49,10 @@ def getIP(mac_address):
 
 ## PINGING Broadcaast to get get mac - ip correlation
 try:
-    kill = lambda process: process.kill()
     ip_addresses = ipaddress.IPv4Network(ipaddress.ip_network(u'{}/{}'.format(netifaces.ifaddresses('br0')[netifaces.AF_INET][0].get("addr"),netifaces.ifaddresses('br0')[netifaces.AF_INET][0].get("netmask")), strict=False))
     logger.info(ip_addresses)
     for ip_addr in ip_addresses:
+        print(ip_addr)
         cmd = ['ping',str(ip_addr),"-w","1"]
         ping = subprocess.Popen(cmd,shell=True,
              stdin=None, stdout=None, stderr=None, close_fds=True)
